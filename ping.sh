b5d480090612323e9bfd01b2a48d2a53203c160e
ctrl-root <muriithidennis340@gmail.com>
2023-03-20 18:28:56 +0300

    added comments

--- ping.sh
+++ ping.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-for ip in $(seq 200 210); do 
-    ping -c 1 192.168.234 $ip |grep "bytes from" |cut -d" " -f 4|cut -d":" -f1 &
+for ip in $(seq 200 210); #This is the ip range
+do 
+    ping -c 1 0.0.0.0 $ip |grep "bytes from" |cut -d" " -f 4|cut -d":" -f1 & #Here you put the ip address that you are attackilg.
+
 done
+#Happy scripting buddy.
